455c9d54d0323f317edd362331971c7f120f8cd5
Simeon <simeon_dionisovsh@abv.bg>
2018-08-06 15:57:56 +0300

    add some text to the readMe

--- 2018.03.19-OOPAdvanced/RetakeExam/Travel/StartUp.cs
+++ 2018.03.19-OOPAdvanced/RetakeExam/Travel/StartUp.cs
@@ -16,15 +16,28 @@
 	{
 		public static void Main(string[] args)
 		{
-			IReader reader = new ConsoleReader();
-			IWriter writer = new ConsoleWriter();
+			//IReader reader = new ConsoleReader();
+			//IWriter writer = new ConsoleWriter();
 
-			IAirport airport = new Airport();
-			IAirportController airportController = new AirportController(airport);
-			IFlightController flightController = new FlightController(airport);
+			//IAirport airport = new Airport();
+			//IAirportController airportController = new AirportController(airport);
+			//IFlightController flightController = new FlightController(airport);
 
-			IEngine engine = new Engine(reader, writer, airportController, flightController);
-			engine.Run();
+			//IEngine engine = new Engine(reader, writer, airportController, flightController);
+			//engine.Run();
+
+			int i = 10;
+			int j = 1;
+			do
+			{
+				j++;
+				if (j > i)
+					break;
+				--i;
+			}
+			while (j < i);
+
+			System.Console.WriteLine("i = " + i + "j = " + j);
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 2018.01.22-2018.04.22-CSharpFundamentals
 ProfetionalModulPart1
+<html>
+<main>
+<H1 color = red> Ei tuka malko 4erven tekst 6e slojim<H2>
+</main>
+</html>
